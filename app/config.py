327560a6832ea57e35913bea1b61323669dc8f1c
moonlightKiR <81652246+moonlightKiR@users.noreply.github.com>
2026-04-26 17:04:46 +0200

    Reworked mess with prs (#3)

--- app/config.py
+++ app/config.py
@@ -6,14 +6,19 @@ from app.constants import DATASET_DIR_NAME
 class AppConfig:
     """Manages dynamic project configuration and paths."""
 
+    """Manages dynamic project configuration and paths."""
+
     @property
     def project_root(self) -> Path:
+        """Returns the project root directory."""
+        return Path(__file__).resolve().parent.parent
         """Returns the project root directory."""
         return Path(__file__).resolve().parent.parent
 
     @property
     def dataset_path(self) -> Path:
-        """Returns the path inside a '.data' folder in the root directory."""
+        """Returns the path inside a '.data'
+        folder in the root directory."""
         return self.project_root / ".data" / DATASET_DIR_NAME
 
     @property
